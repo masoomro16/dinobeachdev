63cbea0c029223c9c06243b6d22dafff8fdc9ab4
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-04 12:50:10 -0700

    added tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,12 @@ module.exports = {
     title: `dinbeach.dev`,
     siteUrl: `https://dinobeach.dev`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", 'gatsby-plugin-postcss', {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/favicon.svg"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
