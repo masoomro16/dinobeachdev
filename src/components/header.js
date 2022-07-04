f87ea6b616e1bb9069b144c2bf100392f7a84edf
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-04 14:26:45 -0700

    added logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Link } from "gatsby"
+import { Link } from "gatsby";
+import { StaticImage } from 'gatsby-plugin-image';
 
 function Header(props) {
     return (
-        <header>
+        <header className='flex'>
+           <StaticImage
+            alt="DinoBeach Logo"
+            src="../images/title-logo.svg"/>
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
