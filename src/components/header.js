ea491f93eb0855cb639bb1258e637b3efbbe41e3
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-04 15:21:01 -0700

    adding header for desktop

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,19 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 function Header(props) {
     return (
-        <header className='flex'>
+        <header className='flex justify-between items-center'>
            <StaticImage
             alt="DinoBeach Logo"
-            src="../images/title-logo.svg"/>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/blog">Blog</Link>
+            src="../images/title-logo.svg"
+            className='scale-50'    
+            />
+            <nav className='flex justify-around'>
+                <Link to="/" className='title-text mx-6 hover:border-[#4fb3fa] hover:border-b-4 no-move'>Home</Link>
+                <Link to="/blog" className='title-text mx-6 hover:border-[#4fb3fa] hover:border-b-4 no-move'>Services</Link>
+                <Link to="/blog" className='title-text mx-6 hover:border-[#4fb3fa] hover:border-b-4 no-move'>Blog</Link>
+                <Link to="/blog" className='title-text mx-6 hover:border-[#4fb3fa] hover:border-b-4 no-move'>Resources</Link>
+                <Link to="/about" className='title-text mx-6 hover:border-[#4fb3fa] hover:border-b-4 no-move'>About</Link>
+                <Link to="/blog" className='title-text mx-6 hover:border-[#4fb3fa] hover:border-b-4 no-move'>Contact</Link>
             </nav> 
         </header>
     );
