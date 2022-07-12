e2e2f96f3e7b9fdf5703f4f2dd9bd581722dbb16
Maira Soomro <masoomro16@ymail.com>
2022-07-12 01:40:03 -0700

    changed picture size and total screen height to 100vh

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
+import {stamp} from "../images/stamp_p.svg"
 
 function Hero() {
   return (
-    <div className='flex flex-wrap md:flex-nowrap justify-center items-center mx-5 md:mx-auto md:max-w-7xl max-w-md'>
+    <div className='h-[calc(100vh_-_150px)] flex flex-wrap md:flex-nowrap justify-center items-center mx-5 md:mx-auto md:max-w-7xl max-w-md'>
         <div className='flex-col'>
             <h1 className='block min-h-[300] title-text md:text-8xl text-4xl font-extrabold  gradient-text pt-2'> creating fun digital experiences</h1>
-            <p>we're a web development team specializing in fast, unique websites</p>
-            <button className='rounded-full bg-[#FFDE9D] md:text-lg  text-sm p-5'>Let us prove it</button>
+            <p className="p-2">we're a web development team specializing in fast, unique websites</p>
+            <button className='rounded-full bg-[#FFDE9D] md:text-lg  text-sm p-5'>find out how</button>
         </div>
+        <img src={stamp} className= 'w-30 h-30'/>
         <div>
-
-        <StaticImage
-                alt="beach"
-                src="../images/vintage-beach2.jpg"
-                className='scale-[.80] rounded-t-[45%] hidden md:block'  
-        />
+          <StaticImage
+                  alt="beach"
+                  src="../images/vintage-beach-short.jpg"
+                  // className='scale-[.80] rounded-t-[45%] hidden md:block'  
+                  className='h-2/5 scale-[.90] rounded-t-[45%] hidden md:block'  
+          />
         </div>
     </div>
   )
