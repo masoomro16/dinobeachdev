4daf893e3cfbefb27c47f5974b98e472c12262e1
Maira Soomro <masoomro16@ymail.com>
2022-07-12 01:48:42 -0700

    added roating stamp

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,13 +4,14 @@ import {stamp} from "../images/stamp_p.svg"
 
 function Hero() {
   return (
-    <div className='h-[calc(100vh_-_150px)] flex flex-wrap md:flex-nowrap justify-center items-center mx-5 md:mx-auto md:max-w-7xl max-w-md'>
-        <div className='flex-col'>
+    <div className='h-[calc(100vh_-_150px)] relative flex flex-wrap md:flex-nowrap justify-center items-center mx-5 md:mx-auto md:max-w-7xl max-w-md'>
+        <div className='flex-col z-20'>
             <h1 className='block min-h-[300] title-text md:text-8xl text-4xl font-extrabold  gradient-text pt-2'> creating fun digital experiences</h1>
             <p className="p-2">we're a web development team specializing in fast, unique websites</p>
             <button className='rounded-full bg-[#FFDE9D] md:text-lg  text-sm p-5'>find out how</button>
         </div>
-        <img src={stamp} className= 'w-30 h-30'/>
+        <StaticImage src="../images/stamp_p.svg" className='absolute bottom-[10%] z-10 rotating'/>
+        {/* <img src={stamp} /> */}
         <div>
           <StaticImage
                   alt="beach"
