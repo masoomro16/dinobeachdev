50d7e903ae79e307a92ff464accc514e8a435eb5
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-29 16:53:01 -0700

    adding footprints

--- src/components/footprint.js
+++ src/components/footprint.js
@@ -3,52 +3,74 @@ import dinoFoot from '../images/trex_footprint.svg';
 
 function Footprint() {
   return (
-    <div class='mt-20 mb-0 p-5'>
+    <div className='mt-20 mb-0 p-5 mx-[6rem]'>
         {/* Top ROW */}
-         <div className='max-h-4 flex justify-between mb-10'>
+         <div className='max-h-4 flex mb-10 justify-between'>
+            {/* Print 1 */}
             <img 
                     src={dinoFoot}
                     alt='dino footprint'
-                    className='rotate-45 object-contain h-16'
+                    className='rotate-[50deg] object-contain h-16 relative top-32'
                 />
+            {/* Print 2 */}
                 <img 
                     src={dinoFoot}
                     alt='dino footprint'
-                    className='rotate-[65deg] object-contain h-16'
+                    className='rotate-[50deg] object-contain h-16 relative right-16 top-16'
                 />
+                {/* Print 3 */}
                 <img 
                     src={dinoFoot}
                     alt='dino footprint'
-                    className='rotate-[135deg] object-contain h-16'
+                    className='rotate-[70deg] object-contain h-16 relative right-20'
                 />
-                <img 
-                    src={dinoFoot}
-                    alt='dino footprint'
-                    className='rotate-90 object-contain h-16'
-                />
-            </div>
 
-            {/* Bottom Row */}
-            <div className='max-h-4 flex justify-between mt-10'>
-            <img 
-                    src={dinoFoot}
-                    alt='dino footprint'
-                    className='rotate-45 object-contain h-16'
-                />
+                {/* Print 4 */}
                 <img 
                     src={dinoFoot}
                     alt='dino footprint'
-                    className='rotate-[65deg] object-contain h-16'
+                    className='rotate-90 object-contain h-16 relative bottom-14 right-20'
                 />
+
+                {/* Print 5 */}
                 <img 
                     src={dinoFoot}
                     alt='dino footprint'
-                    className='rotate-[135deg] object-contain h-16'
+                    className='rotate-[100deg] object-contain h-16 relative right-16'
                 />
+
+                {/* Print 6 */}
                 <img 
                     src={dinoFoot}
                     alt='dino footprint'
-                    className='rotate-90 object-contain h-16'
+                    className='rotate-[100deg] object-contain h-16 relative right-24 bottom-6'
+                />
+
+                {/* Print 7 */}
+                <img 
+                    src={dinoFoot}
+                    alt='dino footprint'
+                    className='rotate-90 object-contain h-16 relative right-16 top-12'
+                />
+                {/* Print 8  */}
+                <img 
+                    src={dinoFoot}
+                    alt='dino footprint'
+                    className='rotate-90 object-contain h-16 relative right-20 top-20'
+                />
+
+                {/* Print 9 */}
+                <img 
+                    src={dinoFoot}
+                    alt='dino footprint'
+                    className='rotate-90 object-contain h-16 relative right-20 top-32'
+                />
+
+                {/* Print 10 */}
+                <img 
+                    src={dinoFoot}
+                    alt='dino footprint'
+                    className='rotate-90 object-contain h-16 relative right-20 top-28'
                 />
             </div>
     </div>
