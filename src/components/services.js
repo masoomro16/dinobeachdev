c1ee51d2890bd72bd1c46988605bd0b1babda68b
Maira Soomro <masoomro16@ymail.com>
2022-07-30 09:58:56 -0700

    updated services tag line

--- src/components/services.js
+++ src/components/services.js
@@ -6,7 +6,7 @@ function Services() {
     <div className='h-screen mx-5 md:mx-28 mt-5 relative z-10'>
         <div className='max-w-[70%] my-3'>
             <h3 className='context-text text-base md:text-xl'>services</h3>
-            <h2 className='text-2xl md:text-5xl title-text gradient-text'>we like to create pretty websites that work fast</h2> 
+            <h2 className='text-2xl md:text-5xl title-text blue-text'>we like to create <span className="gradient-text">pretty</span> websites that work <span className="gradient-text">fast</span></h2> 
             <p>Not quite sure? Take our quiz to find out your dino fit</p>
         </div>
 
