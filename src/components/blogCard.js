65eddcb0e2fb528056e4219419c04e10e71c689c
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-31 15:05:17 -0700

    cleanup and add notes for future

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -5,7 +5,7 @@ function BlogCard(props) {
     <div className={`gradient-background h-80 w-80 md:h-96 md:w-96 rounded-xl my-6 flex justify-center self-center md:${props.custom} shrink flex-grow-1 `} >
         <div className= "h-[97.5%] w-[97.5%] rounded-xl self-center bg-[#FAF6EE]">
           <div className=' h-[90%] w-[90%] mt-[18.725px] ml-[18.725px] flex justify-start items-start rounded-xl'>
-            <h3 className=' text-xl md:text-2xl title-text mx-5 mt-5'>How to make a party game with socket.io and something else</h3>
+            <h3 className=' text-xl md:text-2xl title-text mx-5 mt-5'>{props.title}</h3>
           </div>
         </div>
     </div>
