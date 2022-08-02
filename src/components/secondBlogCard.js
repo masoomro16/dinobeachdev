411aa6d887c9b36bc90e5f32299ef5ecb7521b82
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-08-01 20:01:26 -0700

    starting blog section

---
+++ src/components/secondBlogCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SecondBlogCard() {
+  return (
+    <div className='gradient-background rounded-xl h-80 w-[50%]'>
+        <h1>
+            Second Blog Card Example
+        </h1>
+    </div>
+  )
+}
+
+export default SecondBlogCard
