8f9aea04a706419879071d38acd05082bb4657f4
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-08-06 14:23:52 -0700

    adding blog index basic layout

---
+++ src/components/featuredBlogCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import {FaArrowRight} from 'react-icons/fa';
+
+function FeaturedBlogCard() {
+  return (
+    <div className='gradient-background rounded-xl h-80 md:w-[47%] flex justify-center content-center'>
+      <div className='h-[97.5%] w-[97.5%] rounded-xl justify-self-center self-center'>
+        <div className='h-full w-full flex flex-col justify-between'>
+          <h1 className='text-6xl text-cream mx-4 my-4'>
+              5 CSS Grid Tricks to Learn Today
+          </h1>
+          <FaArrowRight className='self-end arrow mx-4 my-4' size={80}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedBlogCard;
--- src/components/secondBlogCard.js
+++ src/components/secondBlogCard.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 function SecondBlogCard() {
   return (
-    <div className='gradient-background rounded-xl h-80 w-[50%]'>
-        <h1>
+    <div className='gradient-background rounded-xl h-80 md:w-[55%] flex justify-center'>
+        <div className= "h-[95%] w-[97.5%] rounded-xl self-center justify-self-center bg-[#FAF6EE] flex content-center">
+          <h1 className='mx-2 my-2'>
             Second Blog Card Example
-        </h1>
+          </h1> 
+        </div>
     </div>
   )
 }
