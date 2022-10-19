a1376856750b18e8664df59ebd00ffe0e314d429
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-10-19 14:04:34 -0700

    small changes

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -9,7 +9,7 @@ function BlogCard(props) {
 
   return (
     <Link to={`/blog/${props.item?.id}`}>
-    <div className={`gradient-background h-80 w-80 md:h-96 md:w-96 rounded-xl my-6 flex justify-center self-center md:${props.custom} shrink flex-grow-1 `} >
+    <div className={`gradient-background h-80 w-80 md:h-96 md:w-96 rounded-xl my-6 flex justify-center self-center md:${props.custom} shrink flex-grow-1 basis-1/3`} >
         <div className= "h-[97.5%] w-[97.5%] rounded-xl self-center card" onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>
           <div className=' h-[90%] w-[90%] mt-[18.725px] ml-[18.725px] flex  flex-col justify-between rounded-xl'>
             <h3 className=' text-xl md:text-2xl title-text mx-5 mt-5'>{props.item?.title}</h3>
