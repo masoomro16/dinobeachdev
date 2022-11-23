237e1a4733aaa19a712606f59b805ac7f3a943d8
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-11-23 15:48:21 -0800

    added responsivness to services page

--- src/components/ServiceTitle.jsx
+++ src/components/ServiceTitle.jsx
@@ -1,15 +1,38 @@
 import React, {useState} from 'react'
+import {GoTriangleRight, GoTriangleDown} from 'react-icons/go';
+import { serviceData } from '../constants/serviceData';
+import { FaArrowRight } from 'react-icons/fa';
+import { GoPrimitiveDot } from 'react-icons/go';
 
-const ServiceTitle = ({service, selectedService, changeSelectedService}) => {
-  const inactiveTitle = "text-4xl mt-12 text-blue font-bold title-text hover:text-5xl hover:text-pink";
-  const activeTitle = "mt-12 font-bold title-text text-5xl text-pink"
-  console.log(service.title, selectedService);
+const ServiceTitle = ({ service, selectedService, changeSelectedService }) => {
+  const inactiveTitle = "md:text-4xl text-xl mt-12 text-blue font-bold title-text hover:md:text-5xl hover:text-pink";
+  const activeTitle = "mt-12 text-2xl font-bold title-text md:text-5xl text-pink"
+  const arrowStyles = "inline-block h-6 ml-1 md:hidden"
 
 
   return (
-   
-        <h2 className={ selectedService===service.title ? activeTitle : inactiveTitle} onClick={()=>changeSelectedService(service.title)}
+      <div>
+        {/* Title */}
+         <h2 className={ `${selectedService===service.title ? activeTitle : inactiveTitle} inline-block md:block`} onClick={()=>changeSelectedService(service.title)}
         >{service.title}</h2>
+        
+        {/* Mobile arrowStyles */}
+        {selectedService===service.title ? <GoTriangleDown className={arrowStyles} /> : <GoTriangleRight className={arrowStyles} />}
+        
+        
+        {/* Mobile Details */}
+        
+        <div className={`md:hidden content-text ${selectedService === service.title ? 'block' : 'hidden'}`} >
+          {service.details}
+        </div>
+        
+        <div className={`md:hidden content-text mt-4 ${selectedService === service.title ? 'flex' : 'hidden'}`}>
+          <p className='self-center'>Prices Starting at ${service.price}</p>
+          <FaArrowRight className='arrow2 rounded-full h-[7%] w-[7%] p-2 ml-2' />
+        </div>
+
+      </div>
+       
   )
 }
 
