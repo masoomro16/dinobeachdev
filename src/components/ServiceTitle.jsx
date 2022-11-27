f059fd124461932b3f7889c690f557ab3dd1715f
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-11-27 11:24:05 -0800

    refactored services page

--- src/components/ServiceTitle.jsx
+++ src/components/ServiceTitle.jsx
@@ -3,8 +3,6 @@ import React, {useState} from 'react'
 const ServiceTitle = ({service, selectedService, changeSelectedService}) => {
   const inactiveTitle = "text-4xl mt-12 text-blue font-bold title-text hover:text-5xl hover:text-pink";
   const activeTitle = "mt-12 font-bold title-text text-5xl text-pink"
-  console.log(service.title, selectedService);
-
 
   return (
    
